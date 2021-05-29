a23ccc2d0a20d19ad559de1367e2d7177b8159b4
Jake Joseph <jakejosephcs@gmail.com>
2021-05-28 20:04:49 -0400

    Add routing, context and css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import { ContextProvider } from './Context';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ContextProvider>
+      <Router>
+        <App />
+      </Router>
+    </ContextProvider>
   </React.StrictMode>,
+
   document.getElementById('root')
 );
